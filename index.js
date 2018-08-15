262c6a0c5532b54f6edc60da8a376e3ddd40d61b
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 05:39:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
+  Handlebars.registerHelper('displayIngredient', function(ingredients) {
+    return new Handlebars.SafeString(ingredients)
+  });
   // compile and include source
   var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
 }
