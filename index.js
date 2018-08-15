3b34fa68251a5f91755cde25e2191abb314a4163
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 06:19:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function init() {
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
 
-  Handlebars.registerHelper('displayIngredient', function(ingredients) {
-    return new Handlebars.SafeString(ingredients)
-  });
+  // Handlebars.registerHelper('displayIngredient', function(ingredients) {
+  //   return new Handlebars.SafeString(ingredients)
+  // });
   // compile and include source
   var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
   // where and when to generate the template
