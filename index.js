01a39dbfe897605c623a5d9e4c5f95eadc55ef41
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 05:21:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function init() {
   //put any page initialization/handlebars initialization here
   var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
-
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
