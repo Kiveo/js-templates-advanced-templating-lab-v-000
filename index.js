4f94da196cc956d185f411519de7cdd226a7b39f
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 06:22:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function init() {
+  //put any page initialization/handlebars initialization here
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
-   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
-   Handlebars.registerHelper('displayIngredient', function(ingredients) {
-     return new Handlebars.SafeString(ingredients)
-   });
-   var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+
+  Handlebars.registerHelper('displayIngredient', function(ingredients) {
+    return new Handlebars.SafeString(ingredients)
+  });
+  // compile and include source
+  var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
   // where and when to generate the template
   // document.getElementsByTagName("main")[0].innerHTML += formTemplate;
   document.getElementsByTagName('main')[0].innerHTML = formTemplate({'submitAction': 'createRecipe()'});
